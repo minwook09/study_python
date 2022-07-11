e63d12496eda424f5eaf7c0d2c6ae85ac4e7ae1b
minwook <minwook0008@gmail.com>
2022-07-12 01:42:47 +0900

    a0

---
+++ class/app.py
@@ -0,0 +1,12 @@
+class Hero:
+    
+    def __init__(self,skill,skill2):
+        self.q = skill
+        self.w = skill2
+    def hello(self):
+        print('안녕하세요')
+
+nunu = Hero('eat','snowball')
+garen = Hero('strike','shield')
+
+
